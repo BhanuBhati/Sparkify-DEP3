bb1dab0b1cfbb24a5ed5ed02d4fa0eb126982722
Bhanu Bhati <42698017+BhanuBhati@users.noreply.github.com>
2022-03-21 21:58:39 +0530

    General fixes
    Typo fixes

--- sql_queries.py
+++ sql_queries.py
@@ -46,11 +46,11 @@ CREATE TABLE staging_events
     location VARCHAR,
     method VARCHAR,
     page VARCHAR,
-    registration VARCHAR
+    registration VARCHAR,
     sessionId INT,
     song VARCHAR,
     status INT,
-    ts LONGINT,
+    ts BIGINT,
     useragent VARCHAR,
     user_id INT
 )
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS artists
     name VARCHAR NOT NULL,
     location VARCHAR,
     latitude VARCHAR,
-    longitude VARCHAT
+    longitude VARCHAR
 )
 diststyle all
 """)
@@ -188,7 +188,7 @@ VALUES
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
